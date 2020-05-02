9ae11a71ccf9080534993a32d6b0bc908abd40bc
Andres Luque <andresl@biccofarms.com>
2020-05-02 10:08:49 -0500

    Primeras pruebas con cypress

---
+++ cypress/integration/MyTest.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+describe('Validaciones del formulario', () => {
+  it('Valide nombre incorrecto', () => {
+    cy.visit('http://localhost:8080')
+    cy.get('#name').clear().type('An').blur()
+    cy.get("#name").next('.msg-error').should('to.be.visible')
+  })
+  it('Valide nombre correcto', () => {
+    cy.get('#name').clear().type('Andres Luque').blur()
+    cy.get("#name").next('.msg-error').should('to.be.not.visible')
+  })
+
+  it('Validacion de formulario incorrecto', () => {
+    cy.contains("button", "Enviar").click()
+    cy.wait(1000)
+    cy.get("form").children(".alert-danger").should('to.be.visible')
+  });
+  it('Validacion de formulario correcto', () => {
+    cy.fixture("emails.json").as("emails")
+    cy.get("#email").type('@emails[0].email')
+    cy.get("#phone").type('3007393899')
+    cy.contains("button", "Enviar").click()
+    cy.wait(1000)
+    cy.get("form").children(".alert-danger").should('to.be.not.visible')
+  });
+
+});
+
+

a84acea2946b7cf8d4907d0901c7a218092bc0f5
Andres Luque <andresl@biccofarms.com>
2020-05-09 18:42:46 -0500

    Sesion del tema de hooks y fixtures

--- cypress/integration/MyTest.js
+++ cypress/integration/MyTest.js
@@ -1,13 +1,36 @@
 /* eslint-disable no-undef */
 describe('Validaciones del formulario', () => {
-  it('Valide nombre incorrecto', () => {
+  /**
+   * before
+   * beforeEach
+   * after
+   * afterEach
+   * .skip
+   * .only
+   */
+
+  before(() => {
+    cy.log('Before Hook')
     cy.visit('http://localhost:8080')
+    cy.fixture('names.json').as('names')
+  })
+
+  beforeEach(() => {
+    cy.log('BeforeEach Hook')
+  })
+
+  it('Valide nombre incorrecto', () => {
     cy.get('#name').clear().type('An').blur()
     cy.get("#name").next('.msg-error').should('to.be.visible')
   })
-  it('Valide nombre correcto', () => {
-    cy.get('#name').clear().type('Andres Luque').blur()
-    cy.get("#name").next('.msg-error').should('to.be.not.visible')
+
+  it.only('Valide nombre correcto', () => {
+    cy.get("@names").then((names) => {
+      for (let i = 0; i < names.length; i++) {
+        cy.get('#name').clear().type(names[i].name).blur()
+        cy.get("#name").next('.msg-error').should('to.be.not.visible')
+      }
+    })
   })
 
   it('Validacion de formulario incorrecto', () => {
@@ -15,13 +38,22 @@ describe('Validaciones del formulario', () => {
     cy.wait(1000)
     cy.get("form").children(".alert-danger").should('to.be.visible')
   });
-  it('Validacion de formulario correcto', () => {
+  it.skip('Validacion de formulario correcto', () => {
+    cy.get("#name").type('Andres Luque')
     cy.get("#email").type('test@test.com')
     cy.get("#phone").type('3007393899')
     cy.contains("button", "Enviar").click()
     cy.wait(1000)
     cy.get("form").children(".alert-danger").should('to.be.not.visible')
   });
+
+  after(() => {
+    cy.log('After Hook')
+  })
+
+  afterEach(() => {
+    //cy.get("#name").clear()
+  })
 
 });
 
